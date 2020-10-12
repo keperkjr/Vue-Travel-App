ce2a74be2494c8b894ff6be45ef297eba3622c5b
keperkjr <keperkjr@yahoo.com>
2020-10-12 16:44:07 -0700

    Added transitions when clicking on different destinations

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
     <TheNavigation />
-    <router-view :key="$route.path" />
+
+    <!-- <transition name="slide" mode="out-in"> -->
+    <!-- <transition name="moveUp"> -->
+    <transition name="fade">
+      <router-view :key="$route.params.slug" />  
+    </transition>
   </div>
 </template>
 
@@ -22,5 +27,50 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.slide-enter-active,
+.slide-leave-active {
+  transition: opacity 600ms, transform 200ms;
+}
+
+.slide-enter,
+.slide-leave-to {
+  opacity: 0;
+  transform: translateX(-30%);
+}
+
+.moveUp-enter-active {
+  animation: fadeIn 1s ease-in;
+}
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 0.5;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+.moveUp-leave-active {
+  animation: moveUp 0.3s ease-in;
+}
+@keyframes moveUp {
+  0% {
+    transform: translateY(0);
+  }
+  100% {
+    transform: translateY(-400px);
+  }
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
