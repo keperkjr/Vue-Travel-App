3c0f7f7b97afb3f531e57c6941605cb57ee7cc4f
keperkjr <keperkjr@yahoo.com>
2020-10-12 16:24:01 -0700

    Added a 'go back' component

--- src/views/DestinationDetails.vue
+++ src/views/DestinationDetails.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <GoBack />
         <section class="destination">
             <h1>{{destination.name}}</h1>
             <div class="destination-details">
@@ -40,8 +41,12 @@
 
 <script>
 import store from "@/store.js"
+import GoBack from "@/components/GoBack"
 
 export default {
+    components: {
+        GoBack,
+    },
     data() {
         return {
             // destinationId: this.$route.params.id,
